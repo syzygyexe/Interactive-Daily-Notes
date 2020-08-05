ee07da1c4cdb870ec68570327cb770a080f47b27
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-08-05 18:30:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 # encryption
