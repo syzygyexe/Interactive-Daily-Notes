6192c9da15890b7d44bbdf8a1acdb1d581997ad9
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-08-07 17:06:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,29 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from user import UserRegister
-from item import Item, ItemList
+from resources.user import UserRegister
+from resources.item import Item, ItemList
 
 app = Flask(__name__)
+# Turns off Flask_SQLAlchemy tracker, and sets SQLAlchemy modification tracker on.
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # encryption
 app.secret_key = "jose"
 # add api
 api = Api(app)
 
-# JWT creates a new endpoint /login/
-app.config["JWT_AUTH_URL_RULE"] = "/login/"
+# app.config["JWT_AUTH_URL_RULE"] = "/login"
 jwt = JWT(app, authenticate, identity)
+
+# token expiration time
+# # config JWT to expire within half an hour
+# app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+
+
+#Authentication Key Name
+# config JWT auth key name to be 'email' instead of default 'username'
+# app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+
 
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items/")
@@ -23,4 +34,6 @@ api.add_resource(UserRegister, "/register/")
 
 
 if __name__ == "__main__":
+    from models.db import db
+    db.init_app(app)
     app.run(port=5000, debug=False)
