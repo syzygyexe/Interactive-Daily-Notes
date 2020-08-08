73c050f41c3ef4e825251eaf54697062280de653
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-08-08 15:07:35 +0900

    Update security.py

--- security.py
+++ security.py
@@ -1,3 +1,12 @@
 from werkzeug.security import safe_str_cmp
 from models.user import UserModel
 
+from werkzeug.security import safe_str_cmp
+from models.user import UserModel
+
+# Two functions below allow us to retrive users ID or Username without resorting to an iteration.
+def authenticate(username, password):
+    # there is no username in the key, we gonna return None
+    user = UserModel.find_by_username(username)
+    if user and safe_str_cmp(user.password, password):
+        return user
